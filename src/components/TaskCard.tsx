26463fca455399bcab42f6bd71adee784be4fd38
neerajng <neerajnrjng@gmail.com>
2024-06-09 12:43:10 +0530

    fixing touch drag on mobile

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -71,7 +71,7 @@ if(isDragging) {
         className="
         opacity-30
       bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex 
-      text-left rounded-xl border-2 border-rose-500  cursor-grab relative draggable-item"
+      text-left rounded-xl border-2 border-rose-500  cursor-grab relative "
       />
     );
 }
