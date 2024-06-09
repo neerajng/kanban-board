c9d88308004934f25bbf3c7bb1f715fd78cbbda4
neerajng <neerajnrjng@gmail.com>
2024-06-09 12:25:00 +0530

    fixing touch drag on mobile

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -44,7 +44,7 @@ if(editMode) {
     className="bg-mainBackgroundColor p-2.5 h-[100px] 
     min-h-[100px] items-center flex text-left rounded-xl
     hover:ring-2 hover: ring-inset hover: ring-rose-500
-    cursor-grab relative draggable-item">        
+    cursor-grab relative">        
     <textarea
     className="
     h-[90%] w-full resize-none border-none rounded bg-transparent
